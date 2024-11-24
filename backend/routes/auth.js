9f8d34aabc6314ca591f3061dce0a70bbbcf3818
Abhiramhr1 <abhiramhr1.smg@gmail.com>
2024-11-25 00:37:58 +0530

    finally implemented oauth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const{login,register,verifyUser,resendVerificationEmail} = require('../controllers/auth')
+const{login,register,verifyUser,resendVerificationEmail,oauth} = require('../controllers/auth')
 
 router.get('/',(req,res)=>{res.json("calling")});
-router.post('/login',login)
-router.post('/register',register)
-
+router.post('/login',login);
+router.post('/register',register);
+router.post('/Oauth',oauth);
 
 
 router.get('/verify/:token',verifyUser);
